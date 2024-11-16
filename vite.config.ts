25158074128ebeeae1e0826076141bb01cbbc029
Elaine Li <elaine.li@titansoft.com.sg>
2024-11-16 23:52:28 +0800

    feat: add deployment script and gh-pages dependency to package.json

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,16 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+const isProd = process.env.NODE_ENV === 'production'
+const base = isProd ? '/no-worries/' : '/'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
+  base,
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
